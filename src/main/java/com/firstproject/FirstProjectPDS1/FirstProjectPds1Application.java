058c049ddfb7d288ee29a9a3eae97fc42ad85145
gwanderson <wanderson.guilherme@hotmail.com>
2019-08-27 19:50:35 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/firstproject/FirstProjectPDS1/FirstProjectPds1Application.java
@@ -0,0 +1,13 @@
+package com.firstproject.FirstProjectPDS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectPds1Application.class, args);
+	}
+
+}
