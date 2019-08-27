b51718f471479b5d1e688ebe7609552d0120520a
gwanderson <wanderson.guilherme@hotmail.com>
2019-08-27 20:53:13 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/firstproject/FirstProjectPDS1/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.firstproject.FirstProjectPDS1.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstproject.FirstProjectPDS1.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "senha123");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
