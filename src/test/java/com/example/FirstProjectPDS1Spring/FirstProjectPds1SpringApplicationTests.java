b8c8c42120ee6e7bdc27909456bf05ac7995d99a
gwanderson98 <wanderson.guilherme@hotmail.com>
2019-09-17 13:44:31 -0300

    User insert

---
+++ src/test/java/com/example/FirstProjectPDS1Spring/FirstProjectPds1SpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.FirstProjectPDS1Spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstProjectPds1SpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
