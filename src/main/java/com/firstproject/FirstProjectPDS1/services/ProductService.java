97060fc16bbef9fda67c0616ba03d98227014ec9
gwanderson98 <wanderson.guilherme@hotmail.com>
2019-09-24 19:44:43 -0300

    Challenge: CategoryDTO, ProductDTO

--- src/main/java/com/firstproject/FirstProjectPDS1/services/ProductService.java
+++ src/main/java/com/firstproject/FirstProjectPDS1/services/ProductService.java
@@ -2,12 +2,15 @@ package com.firstproject.FirstProjectPDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.firstproject.FirstProjectPDS1.dto.ProductDTO;
 import com.firstproject.FirstProjectPDS1.entities.Product;
 import com.firstproject.FirstProjectPDS1.repositories.ProductRepository;
+import com.firstproject.FirstProjectPDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -15,13 +18,15 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
-	public List<Product> findAll(){
-		return repository.findAll();
+	public List<ProductDTO> findAll(){
+		List<Product> list =  repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 	}	
 	
-	public Product findById(Long id) {
+	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		Product entity =  obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		return new ProductDTO(entity);
 	}
 
 }
