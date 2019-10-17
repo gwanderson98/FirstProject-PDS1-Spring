186630b2fe4326b1fa469240e42af4db546db44e
gwanderson98 <wanderson.guilherme@hotmail.com>
2019-10-17 20:28:03 -0300

    Data model

---
+++ src/main/java/com/firstproject/FirstProjectPDS1/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.firstproject.FirstProjectPDS1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firstproject.FirstProjectPDS1.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
